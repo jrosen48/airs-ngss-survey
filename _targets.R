6dfb81a516b147fe9bece032882d63b863068e32
Joshua Rosenberg <jrosen@msu.edu>
2021-03-08 16:39:22 -0500

    first working pipeline with report

--- _targets.R
+++ _targets.R
@@ -3,16 +3,29 @@ library(targets)
 source(here::here("R", "functions.R"))
 
 # Set target-specific options such as packages.
-tar_option_set(packages = c("here", "tidyverse", "readxl", "janitor"))
+tar_option_set(packages = c("here", "tidyverse", "readxl", "janitor", "rmarkdown"))
 
 # Define targets
 targets <- list(
 
-  tar_target(test_target, test_f),
-  tar_target(another_test_target, test_f)
+  # reading data
+  tar_target(survey_data, read_excel(here("data", "ngss-adoption-survey.xlsx"))),
+  tar_target(state_data, read_excel(here("data", "ngss-adoption-states.xlsx"))),
+
+  # processing data
+  tar_target(joined_data, join_data(survey_data, state_data)),
+  tar_target(joined_data_proc, clean_names(joined_data)),
+  tar_target(joined_data_to_model, process_data(joined_data_proc)),
+
+  # output
+  tar_target(report, rmarkdown::render("report.Rmd",
+                                       output_file = "docs/report.html",
+                                       params = list(data = joined_data_to_model))
+  ),
+  tar_target(site, render_site("docs"))
 
 )
 
 # End with a call to tar_pipeline() to wrangle the targets together.
 # This target script must return a pipeline object.
-tar_pipeline(targets)
+targets
--- r/functions.R
+++ r/functions.R
@@ -1,5 +1,13 @@
 #d <- tar_read(raw_data)
 
-test_f <- function() {
-  "works!"
+join_data <- function(survey_data, state_data) {
+  survey_data %>%
+    rename(location = Location) %>%
+    left_join(state_data)
+}
+
+process_data <- function(data) {
+  data %>%
+    mutate(year = parse_number(dataset)) %>%
+    mutate(location_fct = fct_reorder(location, category_no))
 }
